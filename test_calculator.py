bb078969e608930db713d080f2ecb25228990936
JoachimR <94471243+JoxaDai@users.noreply.github.com>
2024-04-30 18:40:00 +0200

    test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 import calculator
 class TestCalculator:
- def test_addition(self):
- assert 4 == calculator.add(2, 2)
- def test_substraction(self):
- assert 2 == calculator.substract(4, 2)
+ def test_addition():
+    calculator = Calculator()
+    result = calculator.add(2, 2)
+    assert result == 4
